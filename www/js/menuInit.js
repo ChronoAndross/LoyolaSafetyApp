391e604fdcb18614a0e4dd88e5e16fdcb1d40e4f
ChronoAndross <ajgorbaty@loyola.edu>
2014-02-06 13:02:39 -0500

    First commit of project. Most of code is located in index.html file. Pages not implemented yet are map, drinking, and standard info pages.

---
+++ www/js/menuInit.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function() {
+jQuery("#radial_container").radmenu({
+	listClass: 'list', // the list class to look within for items
+	itemClass: 'item', // the items - NOTE: the HTML inside the item is copied into the menu item
+	radius: 100, // radius in pixels
+	animSpeed:400, // animation speed in millis
+	centerX: 30, // the center x axis offset
+	centerY: 100, // the center y axis offset
+	selectEvent: "click", // the select event (click)
+	onSelect: function($selected){ // show what is returned 
+		alert("you clicked on .. " + $selected.index());
+	},
+	angleOffset: 0 // in degrees
+});
+jQuery("#radial_container").radmenu("show");
+});
+	
